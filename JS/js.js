21fdb75861924412f88d1ee81540702be85c6539
Jakub Krajniewski <jakub.krajniewski.programista@gmail.com>
2019-02-04 16:15:16 +0100

    temporary

--- JS/js.js
+++ JS/js.js
@@ -3,15 +3,19 @@ var array = [];               // inicjalizacja tablicy
 
 
 function addItem(event) {
-    
+
+var element = document.getElementById('list');    
 var text = document.getElementById('listItem').value;
 array.push(text);
 
+for (var i = 0; i < array.length; i++) {
 
-var list = '<p>' + array + '</p>'+'</br>';
+    var list = '<p>' + array[i] + '</p>'+'<br / >';
 
-var element = document.getElementById('list');
-element.innerHTML = list;
+    element.innerHTML = list;	
+}
+
+
 
 event.preventDefault();
 }
